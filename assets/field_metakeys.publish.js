da87c5e79cf45e64a984540f8b3f5c2c7ebf2c98
Nils Hörrmann <post@nilshoerrmann.de>
2013-07-08 15:53:51 +0200

    Really make sure that you can focus the input fields
    Without this, the field will always focus the first input independent of the one actually selected. I first thought that this is related to the usage of empty labels but it's not.
    
    This doesn't solve the source of the problem, but at least it makes the field usable again.

--- assets/field_metakeys.publish.js
+++ assets/field_metakeys.publish.js
@@ -1,14 +1,15 @@
 (function($) {
 
 	$(document).on('ready.metakeys', function() {
+
 		$('.metakeys-duplicator').symphonyDuplicator({
 			orderable: true,
 			collapsible: true
-  		}).on('constructstop.duplicator', function(event) {
-  			$(event.target).find('input').on('focus.metakeys click.metakeys', function(event) {
-	  			event.preventDefault();
-	  			event.stopPropagation();
-	  		});
+  		});
+
+  		$('.metakeys-duplicator').on('click.metakeys', 'input:visible', function(event) {
+  			event.preventDefault();
+  			event.stopPropagation();
   		});
 	});
 
