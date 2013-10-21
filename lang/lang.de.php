1238add7f830f91197900a54d7d2862c911d01cb
Nils Hörrmann <post@nilshoerrmann.de>
2013-10-21 15:54:06 +0200

    Update localisation handling in field settings, add German translation

---
+++ lang/lang.de.php
@@ -0,0 +1,60 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Büro für Web- und Textgestaltung',
+			'email' => 'buero@hananils.de',
+			'website' => 'http://hananils.de'
+		),
+		'release-date' => '2013-10-21'
+	);
+
+	/**
+	 * Meta Keys
+	 */
+	$dictionary = array(
+
+		'%s Automatically delete empty keys' => 
+		'%s Leere Angaben automatisch entfernen',
+
+		'\'%s\' is a required field.' => 
+		'\'%s\' ist ein Pflichtfeld.',
+
+		'Add Pair' => 
+		'Neue Angaben',
+
+		'An error occurred while installing %s. %s' => 
+		'Bei der Installation von %s ist ein Fehler aufgetreten. %s',
+
+		'An error occurred while uninstalling %s. %s' => 
+		'Bei der Deinstallation von %s ist ein Fehler aufgetreten. %s',
+
+		'Default Keys' => 
+		'Standardangaben',
+
+		'Key' => 
+		'Titel',
+
+		'Meta Keys' => 
+		'Metaangaben',
+
+		'New Pair' => 
+		'Neue Angaben',
+
+		'Remove Pair' => 
+		'Angaben entfernen',
+
+		'You can optionally assign values by using a double colon: %s.' => 
+		'Sie können optional Angaben mit Doppelpunkten zuweisen: %s.',
+
+		'key::value' => 
+		'titel::wert',
+
+		'If you want to use a comma in your key or value, you need to escape it, e. g. %s.' => 
+		'Kommas innerhalb eines Titels oder Wertes müssen maskiert werden, z. B. %s.',
+
+		'Red\, Green or Blue' => 
+		'rot\, grün oder blau',
+
+	);
